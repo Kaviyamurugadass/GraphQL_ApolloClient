9a71164e798eb063d33ef359db597a699598a574
kaviya-unwita <kaviya@unwita.com>
2025-08-07 17:11:33 +0530

    apollo server

---
+++ apollo-server-demo/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Sample data
+let users = [
+  { id: '1', name: 'Kaviya' },
+  { id: '2', name: 'Murugadass' },
+];
+
+// Type definitions
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+
+  type Mutation {
+    addUser(name: String!): User
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    users: () => users,
+  },
+  Mutation: {
+    addUser: (_, { name }) => {
+      const newUser = { id: String(users.length + 1), name };
+      users.push(newUser);
+      return newUser;
+    },
+  },
+};
+
+// Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
