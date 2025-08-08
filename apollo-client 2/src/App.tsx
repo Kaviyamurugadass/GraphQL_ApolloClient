8aace6b846487cd3eeec8950c812d218e2d954b8
kaviya-unwita <kaviya@unwita.com>
2025-08-08 11:41:23 +0530

    deleted unwanted files, and set websocket apollo server

--- apollo-client 2/src/App.tsx
+++ apollo-client 2/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import RefetchQueries from './components/RefetchQueries';
-import PollInterval from './components/PollInterval';
+// import PollInterval from './components/PollInterval';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-2xl font-bold text-center my-4">User Management</h1>
-      <PollInterval />
+      {/* <PollInterval /> */}
       {/* <RefetchQueries /> */}
     </div>
   );
